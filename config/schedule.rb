8a4e25211e3ad45e632ee2a5f912ee30fb433b47
Italo <italo@MacBook-Pro-de-Italo-2.local>
2018-08-22 15:31:22 -0300

    CronRequest - index.html.erb - Get labels from internationalization file en.yml

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,9 +1,5 @@
 set :output, './log/automagick.cron.log'
-	every 5.hour, :at => '13:00'  do 
+	every 1., :at => '13:00'  do 
 	  runner "Admin::CronRequest.find(1).run!"
-	end	
-
-	every 3.hour, :at => '3:00'  do 
-	  runner "Admin::CronRequest.find(2).run!"
 	end	
 
