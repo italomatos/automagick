2d49c3588ae743eacd7cffd7e84b5c9a8ed95036
Italo <italo@MacBook-Pro-de-Italo-2.local>
2018-08-22 16:13:40 -0300

    change root path

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,7 @@ Rails.application.routes.draw do
     end
   end
   devise_for :users
+
+  root to: "admin/cron_requests#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
