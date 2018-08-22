4aae93a2dbe205d84fc434af7e0686616ddde5b6
Italo <italo@MacBook-Pro-de-Italo-2.local>
2018-08-22 17:39:10 -0300

    added method run to request url

--- app/models/admin/cron_request.rb
+++ app/models/admin/cron_request.rb
@@ -1,7 +1,42 @@
+require 'net/https'
+require 'json'
+require 'logger'
+
 class Admin::CronRequest < ApplicationRecord
 	has_many :cron_request_logs
 	after_save :update_whenever_file
 
+	def run!
+		logger = Logger.new(STDOUT) 
+		uri = URI(self.url)
+		
+		http = Net::HTTP.new uri.host, uri.port
+
+		request = Net::HTTP::Post.new(uri.request_uri)
+
+		if self.http_method == "GET"
+			request = Net::HTTP::Get.new(uri.request_uri)
+		elsif self.http_method == "POST"
+			request = Net::HTTP::Post.new(uri.request_uri)
+		else
+			logger.info("No method error!")
+		end
+
+		request.set_form_data(JSON.parse(self.params)) if not self.params.blank?
+
+		http.read_timeout = 200
+		http.use_ssl = true if self.url.include? "https"
+		http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+	  begin
+	    response = http.request(request)
+	    responseMessage =  response.body
+	    responseCode = response.code.to_i
+	  rescue Exception => e 
+	    responseMessage = e.message
+	    responseCode = -1
+	  end
+		Admin::CronRequestLog.create({cron_request_id: self.id, status_code: responseCode, response:responseMessage})
+	end
 
 	def parse_to_whenever
 	    hour = ""
