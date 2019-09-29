2e4c12d9ed93f33d8abde843bfd5113b4a661d5b
Italo Matos <italomatos@gmail.com>
2019-09-29 08:33:43 -0300

    disable precompile

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,7 +1,6 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
   config.secret_key_base = 'blipblapblup'
-
   # Code is not reloaded between requests.
   config.cache_classes = true
 
@@ -28,7 +27,7 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
@@ -65,7 +64,7 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "automagick_2_#{Rails.env}"
 
   config.action_mailer.perform_caching = false
-  config.serve_static_assets = true
+
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
